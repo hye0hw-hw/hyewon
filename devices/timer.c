f06e9d044edc7f279221c307e659dc81cefc8fad
hye0hw-hw <hye.0hw@gmail.com>
2025-10-30 22:43:40 +0900

    Update timer.c

--- devices/timer.c
+++ devices/timer.c
@@ -87,14 +87,23 @@ timer_elapsed (int64_t then)
 /* Sleeps for approximately TICKS timer ticks.  Interrupts must
    be turned on. */
 void
-timer_sleep (int64_t ticks)
+timer_sleep (int64_t ticks_to_sleep)
 {
-    int64_t start = timer_ticks ();
+  if (ticks_to_sleep <= 0) return;
 
-    ASSERT (intr_get_level () == INTR_ON);
-    while (timer_elapsed (start) < ticks)
-        thread_yield ();
+  enum intr_level old = intr_disable();
+
+  struct thread *cur = thread_current();
+  cur->wake_tick = timer_ticks() + ticks_to_sleep;
+
+  /* 깨어날 시각 오름차순으로 sleep_list에 넣기 */
+  list_insert_ordered (&sleep_list, &cur->elem, wake_tick_less, NULL);
+
+  thread_block();  /* 깨어날 때까지 BLOCKED */
+
+  intr_set_level(old);
 }
+
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
    turned on. */
@@ -133,26 +142,15 @@ timer_mdelay (int64_t ms)
     real_time_delay (ms, 1000);
 }
 
-/* Sleeps for approximately US microseconds.  Interrupts need not
-   be turned on.
 
-   Busy waiting wastes CPU cycles, and busy waiting with
-   interrupts off for the interval between timer ticks or longer
-   will cause timer ticks to be lost.  Thus, use timer_usleep()
-   instead if interrupts are enabled. */
+
 void
 timer_udelay (int64_t us)
 {
     real_time_delay (us, 1000 * 1000);
 }
 
-/* Sleeps execution for approximately NS nanoseconds.  Interrupts
-   need not be turned on.
 
-   Busy waiting wastes CPU cycles, and busy waiting with
-   interrupts off for the interval between timer ticks or longer
-   will cause timer ticks to be lost.  Thus, use timer_nsleep()
-   instead if interrupts are enabled.*/
 void
 timer_ndelay (int64_t ns)
 {
@@ -172,6 +170,7 @@ timer_interrupt (struct intr_frame *args UNUSED)
 {
     ticks++;
     thread_tick ();
+   thread_wake (ticks);
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
